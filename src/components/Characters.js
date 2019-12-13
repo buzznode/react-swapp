bb92aa91b936b32dbdf732310850d96288ac3495
MAN\BDUDERSTAD <brad.duderstadt@coxautoinc.com>
2019-12-13 07:33:41 -0600

    Layout changes;

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
 class Characters extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: ''
+    }
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      searchString: e.target.value
+    });
+  }
+
+  clickHandler = (e) => {
+    e.preventDefault();
+    alert(`got click with ${this.state.searchString}`);
+  }
+
   render() {
     return (
       <div className="content">
@@ -9,9 +27,15 @@ class Characters extends Component {
         </div>
         <form>
           <div className="search-line">
-            Search Criteria
-            <input type='text' onChange={this.myChangeHandler} />
-            <button onClick={this.clickHandler}>Search</button>  
+            <div className="pad-right-1">
+              Search Criteria
+            </div>
+            <div className="pad-right-1">
+              <input type='text' style={{width: '300px'}} onChange={this.changeHandler} />
+            </div>
+            <div>
+              <button onClick={this.clickHandler}>Search</button>  
+            </div>
           </div>
         </form>
       </div>
