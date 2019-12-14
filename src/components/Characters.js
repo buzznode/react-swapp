5334650fc88d04c5e3fad225edc2457304095d82
BRAD-DESKTOP\bradd <brad.duderstadt@gmail.com>
2019-12-14 16:20:30 -0600

    Added Cards for results; Begin addition of new form;

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -12,6 +12,10 @@ function Characters() {
   const [validated, setValidated] = useState(false);
   const [result, loading] = useAsyncHook(query);
 
+  function handleGetAll(e) {
+    e.preventDefault();
+  }
+
   function handleSubmit(e) {
     const form = e.currentTarget;
 
@@ -29,6 +33,7 @@ function Characters() {
   }
 
   function useAsyncHook(criteria) {
+    console.log(`criteria: ${criteria}`);
     const [result, setResult] = useState([]);
     const [loading, setLoading] = useState(false);
 
@@ -38,7 +43,7 @@ function Characters() {
           setLoading(true);
           const response = await SWAPI.get('/people', {
             params: {
-              q: criteria
+              criteria
             }
           });
 
@@ -60,9 +65,6 @@ function Characters() {
 
   return (
     <div className="content">
-      <div className="instructions">
-        Use the interace below to do some stuff with characters.
-      </div>
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Row>
           <Form.Group as={Col} md="4" controlId="validationCustom0">
@@ -72,38 +74,49 @@ function Characters() {
               type="text" 
               style={{width: '300px'}} 
               placeholder="Search string"
-              onChange={e => setSearch(e.target.value)} 
+              // onChange={e => setSearch(e.target.value)} 
+              onChange={e => setSearch(`q: ${e.target.value}`)}
             />
             <Form.Control.Feedback type="invalid">
               Type something, loser
             </Form.Control.Feedback>
           </Form.Group>
+          <Form.Group as={Col} md="4">
+            <Form.Label>Other Getters</Form.Label>
+            <Button onClick={handleGetAll}>Get All Characters</Button>
+          </Form.Group>
         </Form.Row>
         <Button type="submit" variant="outline-warning">Search</Button>
       </Form>
 
-      {
-        loading === false ? (
-          <h1>Search for Characters</h1>
-        ) : loading === null ? (
-          <h1>No Characters Found</h1>
-        ) : (
-          result.map(item => {
-            return (
-              <Card border="warning" style={{ width: '18rem' }}>
-                <Card.Header>{item.name}</Card.Header>
-                <Card.Body>
-                  {/* <Card.Title>Warning Card Title</Card.Title> */}
-                  <Card.Text>
-                    <div>Height: {item.height}</div>
-                    <div>Mass: {item.mass}</div>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            );              
-          })
-        )
-      }
+      <div className="cards-container">
+        {
+          loading === false ? (
+            <h1>Search for Characters</h1>
+          ) : loading === null ? (
+            <h1>No Characters Found</h1>
+          ) : (
+            result.map((item, index) => {
+              return (
+                  <Card border="warning" className="item" key={index}>
+                    <Card.Header>{item.name}</Card.Header>
+                    <Card.Body>
+                      <Card.Text>
+                        <span><b><u>Statistics</u></b></span><br />
+                        <span><b>Height:</b>&nbsp;&nbsp;{item.height}</span><br/>
+                        <span><b>Mass:</b>&nbsp;&nbsp;{item.mass}</span><br/>
+                        <span><b>Birth Year:</b>&nbsp;&nbsp;{item.birth_year}</span><br/>
+                        <span><b>Hair Color:</b>&nbsp;&nbsp;{item.hair_color}</span><br/>
+                        <span><b>Skin Color:</b>&nbsp;&nbsp;{item.skin_color}</span><br/>
+                        <span><b>Eye Color:</b>&nbsp;&nbsp;{item.eye_color}</span><br/>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+              );              
+            })
+          )
+        }
+      </div>
     </div>
   );
 }
